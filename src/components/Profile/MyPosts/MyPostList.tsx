bac6e35170969af555cde88d77bd1ea56edc79f8
uginza <uginza@yandex.ru>
2023-02-23 20:32:09 +0300

    For MyPostList.tsx -onClick added for Add Button -function addPost is set to onClick newPostEl ref is set into textarea

--- src/components/Profile/MyPosts/MyPostList.tsx
+++ src/components/Profile/MyPosts/MyPostList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import s from './MyPosts.module.css';
 import {MyPost} from "./Post/MyPost";
 import {MyProfilePageType} from "../../../redux/state";
@@ -10,12 +10,19 @@ export function MyPostList(props: MyPostsPropsType) {
 
     let myPostsElements = props.myPosts.myPosts.map(p => <MyPost id={p.id} value={p.value} likeCount={p.likeCount}/>)
 
+    let newPostEl = useRef<HTMLTextAreaElement>(null)
+
+    const addPost = () => {
+        if (newPostEl.current !== null) {
+            alert(newPostEl.current.value)
+        }
+    }
 
     return (
         <div className={s.post}>
             <div>My posts</div>
-            <textarea></textarea>
-            <button>Add</button>
+            <textarea ref={newPostEl}></textarea>
+            <button onClick={addPost}>Add</button>
             <div>Send Message</div>
             {myPostsElements}
         </div>
