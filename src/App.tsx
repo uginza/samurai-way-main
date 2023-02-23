a49de7684928c593f3522f4a1c84d780074f9138
uginza <uginza@yandex.ru>
2023-02-23 19:30:04 +0300

    BrowserRouter is set in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Feed} from "./components/Feed/Feed";
 import {Music} from "./components/Music/Music";
@@ -18,7 +18,6 @@ type AppPropsType={
 
 function App(props:AppPropsType) {
     return (
-        <BrowserRouter>
             <div className="appWrapper">
                 <Header/>
                 <NavBar/>
@@ -32,7 +31,6 @@ function App(props:AppPropsType) {
 
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 
