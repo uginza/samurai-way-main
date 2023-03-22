e50b4d9d938922406e7ff25866eeaa6454ab9f2d
uginza <uginza@yandex.ru>
2023-03-22 12:01:41 +0300

    Preloader for login in HeaderContainer.tsx is set

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -4,16 +4,20 @@ import {Header} from "./Header";
 import {connect} from "react-redux";
 import {AppRootStateType} from "../../redux/reduxStore";
 import {setUserData, toggleIsFetching} from "../../redux/authReducer";
+import {Preloader} from "../common/Preloader/Preloader";
 
 export type mapStateToPropsType = {
     isAuth: boolean,
-    login: string
+    login: string,
+    isFetching:boolean
 }
 
 class HeaderContainer extends React.Component<any, any> {
     componentDidMount() {
+        this.props.toggleIsFetching(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
             .then(response => {
+                this.props.toggleIsFetching(false)
                 if (response.data.resultCode === 0) {
                     let {id, login, email} = response.data.data
                     this.props.setUserData(id, login, email)
@@ -23,13 +27,17 @@ class HeaderContainer extends React.Component<any, any> {
     }
 
     render() {
-        return <Header {...this.props} isAuth={this.props.isAuth} login={this.props.login}/>
+        return <>
+            {this.props.isFetching ? <Preloader/> : null}
+            <Header {...this.props} isAuth={this.props.isAuth} login={this.props.login}/>
+        </>
     }
 }
 
-const mapStateToProps = (state: AppRootStateType):mapStateToPropsType => ({
+const mapStateToProps = (state: AppRootStateType): mapStateToPropsType => ({
     isAuth: state.auth.isAuth,
-    login: state.auth.login
+    login: state.auth.login,
+    isFetching:state.auth.isFetching
 })
 
 export default connect(mapStateToProps, {setUserData, toggleIsFetching})(HeaderContainer)
